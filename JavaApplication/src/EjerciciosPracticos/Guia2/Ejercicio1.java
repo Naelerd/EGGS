903f7538a4056631717e78ecdc7dd86c971e4f35
Juan Manuel <juampi_22_55@hotmail.com>
2023-04-02 16:23:59 -0300

    Version 1.3

---
+++ JavaApplication/src/EjerciciosPracticos/Guia2/Ejercicio1.java
@@ -0,0 +1,20 @@
+package EjerciciosPracticos.Guia2;
+
+import java.util.Scanner;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        
+        Scanner teclado = new Scanner(System.in).useDelimiter("\n");
+        int num = 0;
+        
+        System.out.println("Ingrese un numero.");
+        num = teclado.nextInt();
+        
+        if(num % 2 == 0){
+            System.out.println("El numero ingresado es PAR.");
+        } else {
+            System.out.println("El numero ingresado es IMPAR."); 
+        }
+    }
+}
